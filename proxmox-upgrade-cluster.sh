998acc93c51a59ff238adcd3aaace6d2842033f7
Thane Gill <me@thanegill.com>
2026-01-30 13:55:52 -0800

    Create main and process_args functions

--- proxmox-upgrade-cluster.sh
+++ proxmox-upgrade-cluster.sh
@@ -566,189 +566,197 @@ EXAMPLE
 EOF
 }
 
-# Exit here if sourcing for tests
-if [[ -n $ONLY_SOURCE_FUNCTIONS ]]; then return 0; fi
+process_args() {
+  if [[ $# -eq 0 ]]; then
+    log_error "No arguments passed."
+    usage
+    exit 1
+  fi;
 
-if [[ $# -eq 0 ]]; then
-  log_error "No arguments passed."
-  usage
-  exit 1
-fi;
+  while true; do
+    case "$1" in
+      --cluster-node|-c)
+        shift
+        use_cluster_node=true
+        cluster_node="$1"
+        ;;
+      --node|-n)
+        shift
+        cluster_nodes+=("$1")
+        ;;
+      --ssh-user|-u)
+        shift
+        ssh_user="$1"
+        ;;
+      --ssh-opt|-o)
+        shift
+        ssh_options+=("$1")
+        ;;
+      --ssh-allow-password-auth)
+        ssh_key_auth_only=false
+        ;;
+      --cluster-node-use-ip)
+        cluster_node_use_ip=true
+        ;;
+      --dry-run)
+        dry_run=true
+        ;;
+      --pkg-reinstall)
+        shift
+        pkgs_reinstall+=("$1")
+        ;;
+      --force-upgrade)
+        force_upgrade=true
+        ;;
+      --force-reboot)
+        force_reboot=true
+        ;;
+      --no-maintenance-mode)
+        use_maintenance_mode=false
+        ;;
+      --allow-running-guests)
+        allow_running_guests=true
+        ;;
+      --allow-running-tasks)
+        allow_running_tasks=true
+        ;;
+      --jq-bin)
+        shift
+        jq_bin="$1"
+        ;;
+      --verbose)
+        verbose=$((verbose + 1))
+        ;;
+      -+(v))
+        verbose=$((verbose + ${#1} - 1))
+        ;;
+      --help|-h)
+        usage
+        exit 0
+        ;;
+      -*)
+        log_error "$program_name ERROR: unknown option '$1'"
+        log_error "Use $program_name --help for help with command-line options."
+        exit 1
+        ;;
+      *)
+        break
+        ;;
+    esac
+    shift
+  done
 
-while true; do
-  case "$1" in
-    --cluster-node|-c)
-      shift
-      use_cluster_node=true
-      cluster_node="$1"
-      ;;
-    --node|-n)
-      shift
-      cluster_nodes+=("$1")
-      ;;
-    --ssh-user|-u)
-      shift
-      ssh_user="$1"
-      ;;
-    --ssh-opt|-o)
-      shift
-      ssh_options+=("$1")
-      ;;
-    --ssh-allow-password-auth)
-      ssh_key_auth_only=false
-      ;;
-    --cluster-node-use-ip)
-      cluster_node_use_ip=true
-      ;;
-    --dry-run)
-      dry_run=true
-      ;;
-    --pkg-reinstall)
-      shift
-      pkgs_reinstall+=("$1")
-      ;;
-    --force-upgrade)
-      force_upgrade=true
-      ;;
-    --force-reboot)
-      force_reboot=true
-      ;;
-    --no-maintenance-mode)
-      use_maintenance_mode=false
-      ;;
-    --allow-running-guests)
-      allow_running_guests=true
-      ;;
-    --allow-running-tasks)
-      allow_running_tasks=true
-      ;;
-    --jq-bin)
-      shift
-      jq_bin="$1"
-      ;;
-    --verbose)
-      verbose=$((verbose + 1))
-      ;;
-    -+(v))
-      verbose=$((verbose + ${#1} - 1))
-      ;;
-    --help|-h)
-      usage
-      exit 0
-      ;;
-    -*)
-      log_error "$program_name ERROR: unknown option '$1'"
-      log_error "Use $program_name --help for help with command-line options."
+  test $verbose -ge 5 && set -x
+  test $verbose -ge 4 && ssh_options+=("-v")
+
+  ssh_options+=("-l $ssh_user")
+  [[ "$ssh_key_auth_only" == true ]] && ssh_options+=("-o PasswordAuthentication=no")
+
+  if [[ "$use_cluster_node" = true && ${#cluster_nodes[@]} -ne 0 ]]; then
+    log_error "ERROR: Only one of --cluster-node, or --nodes can be used."
+    log_error "See --help for usage."
+    exit 1
+  fi
+
+  if [[ "$use_cluster_node" == false && ${#cluster_nodes[@]} -eq 0 ]]; then
+    log_error "ERROR: One of --cluster-node, or --nodes must be used."
+    log_error "See --help for usage."
+    exit 1
+  fi
+}
+
+main() {
+  process_args "$@"
+
+  # Exit here if sourcing for tests
+  if [[ -n $ONLY_SOURCE_FUNCTIONS ]]; then return 0; fi
+
+  if [[ "$dry_run" == true ]]; then
+    log_warning "Running in dry run mode."
+  fi
+
+  if [[ "$use_cluster_node" = true ]]; then
+    log_status "Getting all cluster nodes from node '$cluster_node'..."
+    if ! is_node_up "$cluster_node"; then
+      log_error "$cluster_node node is currently down."
       exit 1
-      ;;
-    *)
-      break
-      ;;
-  esac
-  shift
-done
-
-test $verbose -ge 5 && set -x
-test $verbose -ge 4 && ssh_options+=("-v")
-
-ssh_options+=("-l $ssh_user")
-[[ "$ssh_key_auth_only" == true ]] && ssh_options+=("-o PasswordAuthentication=no")
-
-if [[ "$use_cluster_node" = true && ${#cluster_nodes[@]} -ne 0 ]]; then
-  log_error "ERROR: Only one of --cluster-node, or --nodes can be used."
-  log_error "See --help for usage."
-  exit 1
-fi
-
-if [[ "$use_cluster_node" == false && ${#cluster_nodes[@]} -eq 0 ]]; then
-  log_error "ERROR: One of --cluster-node, or --nodes must be used."
-  log_error "See --help for usage."
-  exit 1
-fi
-
-if [[ "$dry_run" == true ]]; then
-  log_warning "Running in dry run mode."
-fi
-
-if [[ "$use_cluster_node" = true ]]; then
-  log_status "Getting all cluster nodes from node '$cluster_node'..."
-  if ! is_node_up "$cluster_node"; then
-    log_error "$cluster_node node is currently down."
-    exit 1
+    fi
+    if ! is_node_proxmox "$cluster_node"; then
+      exit 1
+    fi
+    # shellcheck disable=2207
+    cluster_nodes=($(get_cluster_nodes "$cluster_node"))
   fi
-  if ! is_node_proxmox "$cluster_node"; then
-    exit 1
-  fi
-  # shellcheck disable=2207
-  cluster_nodes=($(get_cluster_nodes "$cluster_node"))
-fi
-log_success "Using '${cluster_nodes[*]}' as all nodes to check."
+  log_success "Using '${cluster_nodes[*]}' as all nodes to check."
 
-log_status "Checking if any nodes are not proxmox..."
-if ! check_if_nodes_are_proxmox cluster_nodes; then
-  log_error "At least one node doesn't seem to be proxmox."
-  exit 1
-else
-  log_success "All nodes are proxmox."
-fi
-
-log_status "Checking if any nodes are currently down..."
-if ! all_nodes_up cluster_nodes; then
-  log_error "At least one node is currently down."
-  exit 1
-else
-  log_success "All nodes are up."
-fi
-
-log_status "Checking if any nodes are currently not online..."
-if [[ $(node_get_offline_count "${cluster_nodes[0]}") -ne 0 ]]; then
-  log_error "At least one node is currently not online."
-  exit 1
-else
-  log_success "All nodes are online."
-fi
-
-if [[ "$allow_running_tasks" == true ]]; then
-  log_warning "Not checking for running cluster tasks."
-else
-  log_status "Checking if any nodes currently have tasks running..."
-  if any_nodes_running_tasks cluster_nodes; then
-    log_error "At least one node is currently running tasks."
+  log_status "Checking if any nodes are currently down..."
+  if ! all_nodes_up cluster_nodes; then
+    log_error "At least one node is currently down."
     exit 1
   else
-    log_success "No tasks are running."
+    log_success "All nodes are up."
   fi
-fi
 
-log_status "Checking for updates on all nodes..."
-apt_update_nodes cluster_nodes
+  log_status "Checking if any nodes are not proxmox..."
+  if ! all_nodes_proxmox cluster_nodes; then
+    log_error "At least one node doesn't seem to be proxmox."
+    exit 1
+  else
+    log_success "All nodes are proxmox."
+  fi
 
-if [[ "$force_upgrade" == true ]]; then
-  upgrade_nodes=("${cluster_nodes[@]}")
-  log_warning "Forcing upgrade for all nodes, not just those that have updates available."
-else
-  # shellcheck disable=2207
-  upgrade_nodes=($(get_nodes_upgradeable cluster_nodes))
-fi
+  log_status "Checking if any nodes are currently not online..."
+  if [[ $(node_get_offline_count "${cluster_nodes[0]}") -ne 0 ]]; then
+    log_error "At least one node is currently not online."
+    exit 1
+  else
+    log_success "All nodes are online."
+  fi
 
-if [[ "$use_maintenance_mode" == false ]]; then
-  log_warning "Not using maintenance mode when upgrading."
-fi
+  if [[ "$allow_running_tasks" == true ]]; then
+    log_warning "Not checking for running cluster tasks."
+  else
+    log_status "Checking if any nodes currently have tasks running..."
+    if any_nodes_running_tasks cluster_nodes; then
+      log_error "At least one node is currently running tasks."
+      exit 1
+    else
+      log_success "No tasks are running."
+    fi
+  fi
 
-if [[ ${#upgrade_nodes[@]} -eq 0 ]]; then
-  log_success "No nodes need updates. Exiting."
-  exit 0
-fi
+  log_status "Checking for updates on all nodes..."
+  apt_update_nodes cluster_nodes
 
-log_success "Using '${upgrade_nodes[*]}' as node upgrade sequence."
+  if [[ "$force_upgrade" == true ]]; then
+    upgrade_nodes=("${cluster_nodes[@]}")
+    log_warning "Forcing upgrade for all nodes, not just those that have updates available."
+  else
+    # shellcheck disable=2207
+    upgrade_nodes=($(get_nodes_upgradeable cluster_nodes))
+  fi
 
-for node in "${upgrade_nodes[@]}"; do
-  log_success "[$node] Starting upgrade."
-  node_pre_upgrade "$node"
-  node_upgrade "$node"
-  node_reboot "$node"
-  node_post_upgrade "$node"
-  log_success "[$node] Successfully upgraded."
-done
+  if [[ "$use_maintenance_mode" == false ]]; then
+    log_warning "Not using maintenance mode when upgrading."
+  fi
 
-log_success "Nodes '${upgrade_nodes[*]}' successfully upgraded."
+  if [[ ${#upgrade_nodes[@]} -eq 0 ]]; then
+    log_success "No nodes need updates. Exiting."
+    exit 0
+  fi
+
+  log_success "Using '${upgrade_nodes[*]}' as node upgrade sequence."
+
+  for node in "${upgrade_nodes[@]}"; do
+    log_success "[$node] Starting upgrade."
+    node_pre_upgrade "$node"
+    node_upgrade "$node"
+    node_reboot "$node"
+    node_post_upgrade "$node"
+    log_success "[$node] Successfully upgraded."
+  done
+
+  log_success "Nodes '${upgrade_nodes[*]}' successfully upgraded."
+}
+
+main "$@"
